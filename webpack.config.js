3133109952048f5a78d0c75eaaa0b902fde42868
Viktor Wahlberg <vikwa339@student.liu.se>
2018-10-25 15:27:41 +0200

    Added basic webpack dev server configuration

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    output:{
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist/')
+    },
+    devtool: 'source-map',
+    devServer:{
+        contentBase: 'dist'
+    },
+    mode: 'development'
+};
